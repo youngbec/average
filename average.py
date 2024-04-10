42118918a83f07cea6144e64696b1241d777abf0
topher346 <christopher.l.youngberg@hotmail.com>
2024-04-09 23:07:35 -0700

    Project 2a

--- average.py
+++ average.py
@@ -1,7 +1,13 @@
-print(input("Please enter five numbers: "))
+print("Please enter five numbers.")
 num_1 = -2.4
 num_2 = 5.1
 num_3 = 6.0
 num_4 = 123.8
 num_5 = -19
-print("the average of those numbers is: ", ((num_1 + num_2 + num_3 + num_4 + num_5)/5))
+print(num_1)
+print(num_2)
+print(num_3)
+print(num_4)
+print(num_5)
+print("the average of those numbers is: ", )
+print(((num_1 + num_2 + num_3 + num_4 + num_5)/5))
